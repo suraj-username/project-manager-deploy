a18abd4d308f58258fe992c5601d2a7e00078124
sreeks7675 <sreekarkashyap2210083@ssn.edu.in>
2025-11-27 01:35:10 +0530

    Connection Pooling for improved user handling as comppared to singleton connection.

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,17 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  try {
+  /*try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);*/
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      // --- Connection Pooling Options ---
+      maxPoolSize: 10, 
+      minPoolSize: 2, 
+      socketTimeoutMS: 45000,
+    });
     
   } catch (error) {
     console.error(`Error: ${error.message}`);
